c15e74ea5e37923806885d7e7b5f561a7d336c04
한승민 <hanseungmin@proton.me>
2025-05-28 11:02:08 +0900

    astro rewrite

---
+++ src/content/config.ts
@@ -0,0 +1,26 @@
+import { glob } from "astro/loaders";
+import { defineCollection, reference, z } from "astro:content";
+
+const blog = defineCollection({
+    loader: glob({ pattern: '**/*.md', base: "blog" }),
+    schema: z.object({
+        title: z.string(),
+        description: z.string(),
+        date: z.coerce.date(),
+        authors: z.array(reference("authors")),
+        updatedDate: z.coerce.date().optional(),
+        banner: z.string().optional(),
+    }),
+});
+
+const authors = defineCollection({
+    loader: glob({ pattern: '**/*.json', base: "authors" }),
+    schema: z.object({
+        name: z.string(),
+        avatar: z.string(),
+        url: z.string(),
+    })
+});
+
+
+export const collections = { blog, authors };
